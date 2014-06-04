bf3c86a721a16a518a45091782a31856b33c8969
sergesyrota <serg.syrota@gmail.com>
2014-06-03 22:14:02 -0500

    Changed security of the alert email gateway, as security by obscurity is no security. Especially when you publish it on GitHub :P

--- nagios/notificationCommand.py
+++ nagios/notificationCommand.py
@@ -3,16 +3,18 @@
 import sys
 import urllib
 import urllib2
-import hashlib
 
 if len(sys.argv) != 3:
     print "USAGE: notificationCommand.py <to> <message>"
     exit(1)
 
+with open('/etc/nagios/alert.secret', 'r') as content_file:
+    token = content_file.read()
+
 message = sys.argv[2].replace('\\n', "\n");
 data = {'to': sys.argv[1],
         'message': message,
-        'token': hashlib.md5(message).hexdigest()}
+        'token': token}
 
 response = urllib2.urlopen('http://www.syrota.com/houseAlert.php', urllib.urlencode(data))
 #print(response.read())
