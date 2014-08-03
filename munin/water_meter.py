f06049f4d589d3772015005e49a1c15bacf6b097
sergesyrota <serg.syrota@gmail.com>
2014-08-03 14:48:16 -0500

    Updated water meter Munin plugin to show Gallons per hour, instead of simple counter.

--- munin/water_meter.py
+++ munin/water_meter.py
@@ -14,15 +14,18 @@ if len(sys.argv) > 1 and sys.argv[1] == "config":
     print "graph_title Water meter"
     print "graph_args --base 1000"
     print "graph_category sensors"
-    print "graph_info Shows number of half-rotations of water meter disc"
-    print "graph_vlabel units"
-    print "units.label Rotations per second"
-    print "units.type DERIVE"
-    print "units.min 0"
+    print "graph_info Shows water usage rate"
+    print "graph_vlabel gph"
+    print "gph.label Gallons per hour"
+    print "gph.type DERIVE"
+    # Original value is per second. One half-rotation is 20mL. 20ml/second * 3600 = 20ml/hour; 1 gal = 3785.41ml;
+    # to get gallons perhour, multiply by 3600 * 20/3785.41 = 19.020396733775205327824462871921
+    print "gph.cdef gph,19.02,*"
+    print "gph.min 0"
     exit(0)
 
 try:
-    print 'units.value ' + rs485.command('WtrMn', 'getCount')
+    print 'gph.value ' + rs485.command('WtrMn', 'getCount')
 except Exception as e:
     print >> sys.stderr, e
     exit(1)
