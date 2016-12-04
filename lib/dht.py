b1086f91105d663bbf9978d49afa60fca49996c7
Sergey Syrota <serg.syrota@gmail.com>
2016-12-04 15:59:12 -0600

    Moved DHT module into the lib, adding support for JSON format. Fixing munin data labels, and making colors more easily seen on white background (for target temp)

--- lib/dht.py
+++ lib/dht.py
@@ -2,9 +2,36 @@ __author__ = 'sergey'
 
 import rs485
 import re
+import json
 
 class dhtException(Exception):
     pass
+
+def ctof(deg):
+    return (32+(float(deg)*9/5))
+
+def ftoc(deg):
+    return ((float(deg) - 32)*5/9)
+
+def getJsonDht(device, command, ageLimit=300):
+    res = rs485.command(device, command)
+    if (res.startswith("ERROR")):
+        raise dhtException('Error polling ' + device + ':' + command + ' ' + res)
+    data = json.loads(res)
+    if (data['age'] > ageLimit):
+        raise dhtException("DHT data is too old: {0        } seconds".format(data['age']))
+    # Normalize unit of measure to Celsius
+    temp = float(data['t'])
+    if (data['unit'] == 'C*100'):
+        temp = temp/100.0
+    elif (data['unit'] == 'F'):
+        temp = ftoc(temp)
+    return {
+        'temperature': temp,
+        'fahrenheit': ctof(temp),
+        'humidity': data['rh'],
+        'age': data['age']
+        }
 
 def getDht(device, command):
     text = rs485.command(device, command)
@@ -14,7 +41,7 @@ def getDht(device, command):
         raise dhtException("Result too old (%d seconds); Discarding" % int(res[0]))
     return {
         'temperature': float(res[0]),
-        'fahrenheit': (32+(float(res[0])*9/5)),
+        'fahrenheit': ctof(float(res[0])),
         'humidity': res[1],
         'age': res[2]
         }
--- munin/bedroom-temperature.py
+++ munin/bedroom-temperature.py
@@ -4,21 +4,9 @@ import sys
 sys.path.insert(0, '/var/www/home/py/lib')
 import rs485
 import json
+import dht
 import subprocess
 import urllib2
-
-def cToF(degrees):
-    return (32+(degrees*9/5))
-
-def getTemperature(device, command):
-    res = rs485.command(device, command)
-    if (res.startswith("ERROR")):
-        raise Exception('Error polling ' + device + ':' + command + ' ' + res)
-    data = json.loads(res)
-    if (data['age'] < 300):
-        return data['t']/100.0  # Unit is C*100
-    else:
-        raise Exception("DHT data is too old: {0        } seconds".format(data['age']))
 
 if len(sys.argv) > 1 and sys.argv[1] == "autoconf":
     print "yes"
@@ -34,27 +22,28 @@ if len(sys.argv) > 1 and sys.argv[1] == "config":
     print "master_current.label Master (current)"
     print "master_current.colour 2c7bf9"
     print "master_target.label Master (target)"
-    print "master_target.colour cee1ff"
+    print "master_target.colour 87b5ff"
     print "middle_current.label Middle (current)"
-    print "middle_current.colour f99d2c"
+    print "middle_current.colour c67e01"
     print "middle_target.label Middle (target)"
-    print "middle_target.colour f7d1a3"
+    print "middle_target.colour efb04a"
     exit(0)
 
 try:
     # Master bedroom
-    print('master_curret.value {0:4.1f}'.format(cToF(getTemperature('EnvMaster', 'getDht'))))
+    data = dht.getJsonDht('EnvMaster', 'getDht')
+    print('master_current.value {0:4.1f}'.format(data['fahrenheit']))
     p = subprocess.Popen('php /home/sergey/temps/hvac-zoning/server/cron-bedroom-controller.php getTarget', shell=True, stdout=subprocess.PIPE)
     data = p.stdout.read()
-    print('master_target.data {0:4.1f}'.format(cToF(float(data))))
+    print('master_target.value {0:4.1f}'.format(dht.ctof(float(data))))
     # Middle bedroom
     res = urllib2.urlopen('http://192.168.8.90/tstat/')
     data = json.loads(res.read())
-    print('middle_current.data {0}'.format(data['temp']))
+    print('middle_current.value {0}'.format(data['temp']))
     if (data['tmode'] == 2):
-        print('middle_target.data {0}'.format(data['t_cool']))
+        print('middle_target.value {0}'.format(data['t_cool']))
     else:
-        print('middle_target.data {0}'.format(data['t_heat']))
+        print('middle_target.value {0}'.format(data['t_heat']))
 except Exception as e:
     print >> sys.stderr, e
     exit(1)
